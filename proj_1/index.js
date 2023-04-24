49cffd8240c5787cb8ab2268cce6e15f2b6927e9
Shashwat Kaundinya <shashwat_247@yahoo.com>
2023-04-24 09:17:45 +0530

    skipShort

--- proj_1/index.js
+++ proj_1/index.js
@@ -14,6 +14,28 @@ srv.add({ component: 'greeter' }, (msg, respond) => {
     respond(null, { message: 'Hello ' + msg.name });
 });
 
+srv.add({ cmd: 'wordcount' }, (msg, respond) => {
+    const length = msg.phrase.split(' ').length;
+    respond(null, { words: length });
+});
+
+srv.add({ cmd: 'wordcount', skipShort: true }, (msg, respond) => {
+    const words = msg.phrase.split(' ');
+    let validWords = 0;
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length > 3) {
+            validWords++;
+        }
+    }
+    respond(null, { words: validWords });
+});
+
+
+
+// actions
+
+
+
 srv.act({ role: 'math', cmd: 'sum', left: 1, right: 2 }, (err, data) => {
     if (err) {
         return console.error(err);
@@ -22,11 +44,17 @@ srv.act({ role: 'math', cmd: 'sum', left: 1, right: 2 }, (err, data) => {
 });
 srv.act({ role: 'math', cmd: 'product', left: 3, right: 4 }, console.log);
 
-srv.act({ component: 'greeter', name: 'Shashwat' },
-    (error, response) => {
-        if (error) return console.log(error);
-        console.log(response.message);
-    });
+srv.act({ component: 'greeter', name: 'Shashwat' }, (error, response) => {
+    if (error) return console.log(error);
+    console.log(response.message);
+});
 
 
+srv.act({ cmd: 'wordcount', phrase: 'Hello world this is Seneca' }, (err, response) => {
+    console.log(response);
+});
+
+srv.act({ cmd: 'wordcount', skipShort: true, phrase: 'Hello world this is Seneca' }, (err, response) => {
+    console.log(response);
+});
 
